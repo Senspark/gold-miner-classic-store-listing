d37af3916f1ada9516ab2dc97ac502a4bf08950b
Lam Ho <lam@senspark.com>
2016-06-16 09:41:21 +0700

    Initial commit

---
+++ gplay/run_populate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os, os.path
+
+cmd = "../../app-stores-toolkit/populate.py metadata -platform android -prj-path . -data-file-path ../src/goldminer-metadata-translate.xlsx -customized-metadata-path ../src/gplay/metadata"
+print cmd
+os.system(cmd)
