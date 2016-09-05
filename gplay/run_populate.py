14b89d4889234cb9deae4cc02fa9309d0206d4a6
Nikel Arteta <nikelarteta@Senspark-Mini-04s-Mac-mini.local>
2016-09-05 11:17:06 +0700

    - Stop tracking files. - Update gitignore.

--- gplay/run_populate.py
+++ gplay/run_populate.py
@@ -4,6 +4,7 @@
 import sys
 import os, os.path
 
-cmd = "../../app-stores-toolkit/populate.py metadata -platform android -prj-path gplay -data-file-path src/goldminer-metadata-translate.xlsx -customized-metadata-path src/gplay/metadata"
+cmd = "../../../app-stores-toolkit/populate-v2.py metadata -platform android -prj-path . -sheet 1f8JS4pz6xhCTWdnqNUmpbYyvvt95dBUqmf6xj_rG2a0 -customized-metadata-path ../src/gplay/metadata"
+
 print cmd
 os.system(cmd)
