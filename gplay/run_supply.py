ed8540b0287a08d3dd653895a8a4680815182c17
Nikel Arteta <nikelarteta@Thanhs-Mac-mini.local>
2016-10-17 11:05:14 +0700

    - Change json file.

--- gplay/run_supply.py
+++ gplay/run_supply.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = 'supply -j ../../../Google-Play-Android-Developer-394f15d83386.json -a beta -p com.senspark.goldminerclassic --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
+cmd = 'supply -j ../../../THANH-Google-Play-Android-Developer-b8ad63fc6d69.json -a beta -p com.senspark.goldminerclassic --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
 print cmd
 os.system(cmd)
